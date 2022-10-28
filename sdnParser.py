f077b2826ee8f93f90db6d305553073e08abeee4
jebryce <jbryce@hawaii.edu>
2022-10-27 20:59:27 -1000

    added some documentation, added dictionary lookups

--- sdnParser.py
+++ sdnParser.py
@@ -1,29 +1,70 @@
+import EtherType_dict
+
 class Packet:
     def __init__(self, packet):
+        # packet is a bytes object
         self.packet = packet
-        self.destination_mac_address = self.packet[0:6]
-        self.source_mac_address = self.packet[6:12]
-        self.findEtherType()
 
-        
+        # ex: b'\xff\xff\xff\xff\xff\xff' (type: bytes)
+        # in hex this would be: FF:FF:FF:FF:FF:FF
+        self.destination_mac_address = packet[0:6]
+        self.source_mac_address = packet[6:12]
 
-
-    def findEtherType(self):
-        #Check if tagged traffic
-        if self.packet[12:14].hex()=='8100':
+        # Check if tagged traffic 
+        # 8100 is the EtherType 802.1Q (aka tagged traffic)
+        if packet[12:14].hex()=='8100':
+            # Decided to use a boolean variable as it is either tagged or not 
             self.tagged = True
-            self.vlan_id = self.packet[14:16]
-            self.i = 4
-            
+
+            # ex: 0003 (type: bytes)
+            self.vlan_id = packet[14:16]
+
+            # the rest of the packet, this is to avoid a seperate variable to 
+            # keep track of the differing index due to the 802.1Q header
+            etherTypePacket = packet[16:]
         else:
             self.tagged = False
-            self.i = 0
-        self.ethertype = self.packet[12+self.i:14+self.i]
+            etherTypePacket = packet[12:]
 
+
+        self.findEtherType(etherTypePacket)
+
+
+    def findEtherType(self, partialPacket):
+        # decided to make this a seperate function in case we get a packet with 
+        # an Ether Type I haven't made a class for yet
+
+        # ex: 0806 (type: bytes)
+        self.ethertype = partialPacket[0:2]
+
+        # looks up ethertype value in dictionary to find description
+        # ex: 'Address Resolution Protocol (ARP)' (type: string)
+        self.ethertype_desc = EtherType_dict.ethertypes.get(
+            self.ethertype.hex()
+        )                                            
+        # if the key in dictionary.get(key) does not exist, it returns None
+
+
+        # this will probably never get called, but essentially does the same as 
+        # previous method, but for rarer ranged ethertypes.
+        if self.ethertype_desc == None:
+            for key in EtherType_dict.ranged_ethertypes:
+
+                # if the upper value of the range is greater than the ethertype
+                # this works as the dictionary is in incremental order
+                if bytes.fromhex(key[5:]) >= self.ethertype:
+
+                    # \ character indicates expression is on next line
+                    self.ethertype_desc = \
+                        EtherType_dict.ranged_ethertypes.get(key)
+                    break
+            
+    
+        # will replace laters
         if self.ethertype.hex() == '0806':
-            self.arp = arp(self.packet[14+self.i:])
+            self.arp = arp(partialPacket[2:])
         elif self.ethertype.hex() == '0800':
-            self.ipv4 = ipv4(self.packet[14+self.i:])
+            self.ipv4 = ipv4(partialPacket[2:])
 
 class arp(Packet):
     def __init__(self, arp_packet):
