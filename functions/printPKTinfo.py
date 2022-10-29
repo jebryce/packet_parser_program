9b0af72f61d48ce0b1535f56da83a26c34fcecd9
jebryce <jbryce@hawaii.edu>
2022-10-28 16:36:57 -1000

    partial readability increase

--- functions/printPKTinfo.py
+++ functions/printPKTinfo.py
@@ -1,21 +1,38 @@
 import hexdump
 import os
 def createPrintList(pkt_object):
-    
     # bar_length is the number of characters wide to print the table row borders
-    bar_length = 152
-    
+    bar_length = 100
+
     # This function populates a list full of strings
     printList = []
+
+    def print_address(type = None, data = None, desc = None):
+        pkt_address_table = "  {type:>11} | {data:^17} | {desc}"
+        return printList.append(pkt_address_table.format(
+                type = type, data = data, desc = desc
+            ))
+    
+    def print_address_line():
+        address_line = 14*'-'+'+'+19*'-'+'+'+(bar_length-35)*'-'
+        return printList.append(address_line)
+
+    def print_info(type = None, data = None, desc = None):
+        pass
+
+    def print_info_line():
+        info_line = 14*'-'+'+'+32*'-'+'+'+(bar_length-48)*'-'
+        return printList.append(info_line)
+
 
 
     # Initialize string formats
     bar = bar_length*'%'
-    pkt_table = "  {type:>11} | {dataType:^17} | {description}"
-    pkt_table_line = 14*'-'+'+'+19*'-'+'+'+(bar_length-35)*'-'
+    
+
     etherType_table_line = 14*'-'+'+'+32*'-'+'+'+(bar_length-48)*'-'
-    etherType_table = "  {type:>11} | {data:<30} | {description}"
-    etherTypedata_table = "  {type:>11} |--> {data:<27} | {description}"
+    etherType_table = "  {type:>11} | {data:<30} | {desc}"
+    etherTypedata_table = "  {type:>11} |--> {data:<27} | {desc}"
 
 
     # Honestly am surprised this worked
@@ -27,46 +44,46 @@ def createPrintList(pkt_object):
 
     # Print MAC address info
     printList.append(bar)
-    printList.append(pkt_table.format(type = '', dataType = 'MAC Address', description='Vendor ID'))
-    printList.append(pkt_table_line)
-    printList.append(pkt_table.format(type = 'Source', dataType = pkt_object.source_mac_address.hex(':').upper(), description = ''))
-    printList.append(pkt_table.format(type = 'Destination', dataType = pkt_object.destination_mac_address.hex(':').upper(), description = ''))
+    print_address(type = '', data = 'MAC Address', desc='Vendor ID')
+    print_address_line()
+    print_address(type = 'Source', data = pkt_object.source_mac_address.hex(':').upper(), desc = '')
+    print_address(type = 'Destination', data = pkt_object.destination_mac_address.hex(':').upper(), desc = '')
 
     
     # Print EtherType info
     printList.append(bar)
-    printList.append(etherType_table.format(type = 'EtherType', data = 'Value (numbers are in hex)', description = 'Description'))
+    printList.append(etherType_table.format(type = 'EtherType', data = 'Value (numbers are in hex)', desc = 'desc'))
     printList.append(etherType_table_line)
 
     # Print if Tagged traffic
     if pkt_object.tagged == True:
-        printList.append(etherType_table.format(type = '802.1Q', data = '8100', description = ''))
-        printList.append(etherTypedata_table.format(type = '', data = 'VLAN ID: '+pkt_object.vlan_id.hex().upper().lstrip('0'), description = ''))
+        printList.append(etherType_table.format(type = '802.1Q', data = '8100', desc = ''))
+        printList.append(etherTypedata_table.format(type = '', data = 'VLAN ID: '+pkt_object.vlan_id.hex().upper().lstrip('0'), desc = ''))
         printList.append(etherType_table_line)
 
     # Print if ARP packet
     if pkt_object.ethertype.hex() == '0806':
         # Print ARP stuff
-        printList.append(etherType_table.format(type = 'ARP', data = '0806', description = pkt_object.ethertype_desc))
-        printList.append(etherTypedata_table.format(type = '', data = 'Hardware Type: '+pkt_object.arp.hardware_type.hex().upper().lstrip('0'), description = ''))
-        printList.append(etherTypedata_table.format(type = '', data = 'Protocol Type: '+pkt_object.arp.protocol_type.hex().upper(), description = ''))
-        printList.append(etherTypedata_table.format(type = '', data = 'Hardware Size: '+pkt_object.arp.hardware_size.hex().upper().lstrip('0'), description = ''))
-        printList.append(etherTypedata_table.format(type = '', data = 'Protocol Size: '+pkt_object.arp.protocol_size.hex().upper().lstrip('0'), description = ''))
-        printList.append(etherTypedata_table.format(type = '', data = 'Opcode: '+pkt_object.arp.opcode.hex().upper().lstrip('0'), description = ''))
+        printList.append(etherType_table.format(type = 'ARP', data = '0806', desc = pkt_object.ethertype_desc))
+        printList.append(etherTypedata_table.format(type = '', data = 'Hardware Type: '+pkt_object.arp.hardware_type.hex().upper().lstrip('0'), desc = ''))
+        printList.append(etherTypedata_table.format(type = '', data = 'Protocol Type: '+pkt_object.arp.protocol_type.hex().upper(), desc = ''))
+        printList.append(etherTypedata_table.format(type = '', data = 'Hardware Size: '+pkt_object.arp.hardware_size.hex().upper().lstrip('0'), desc = ''))
+        printList.append(etherTypedata_table.format(type = '', data = 'Protocol Size: '+pkt_object.arp.protocol_size.hex().upper().lstrip('0'), desc = ''))
+        printList.append(etherTypedata_table.format(type = '', data = 'Opcode: '+pkt_object.arp.opcode.hex().upper().lstrip('0'), desc = ''))
         
         # ARP provides MAC address as well, is different when Opcode = 1 (request)
         printList.append(bar)
-        printList.append(pkt_table.format(type = '', dataType = 'MAC Address', description='Vendor ID'))
-        printList.append(pkt_table_line)
-        printList.append(pkt_table.format(type = 'ARP Sender', dataType = pkt_object.arp.sender_mac_address.hex(':').upper(), description = ''))
-        printList.append(pkt_table.format(type = 'ARP Target', dataType = pkt_object.arp.target_mac_address.hex(':').upper(), description = ''))
-        printList.append(pkt_table_line)
+        print_address(type = '', data = 'MAC Address', desc='Vendor ID')
+        print_address_line()
+        print_address(type = 'ARP Sender', data = pkt_object.arp.sender_mac_address.hex(':').upper(), desc = '')
+        print_address(type = 'ARP Target', data = pkt_object.arp.target_mac_address.hex(':').upper(), desc = '')
+        print_address_line()
 
         # IP address
-        printList.append(pkt_table.format(type = '', dataType = 'IP Address', description='Location'))
-        printList.append(pkt_table_line)
-        printList.append(pkt_table.format(type = 'ARP Sender', dataType = bytes2ip.format(*pkt_object.arp.sender_ip_address), description = ''))
-        printList.append(pkt_table.format(type = 'ARP Target', dataType = bytes2ip.format(*pkt_object.arp.target_ip_address), description = ''))
+        print_address(type = '', data = 'IP Address', desc='Location')
+        print_address_line()
+        print_address(type = 'ARP Sender', data = bytes2ip.format(*pkt_object.arp.sender_ip_address), desc = '')
+        print_address(type = 'ARP Target', data = bytes2ip.format(*pkt_object.arp.target_ip_address), desc = '')
 
     
         printList.append(bar)  
