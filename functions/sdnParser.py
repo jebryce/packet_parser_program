65432823de87a9574be05558b2930b73ea296ade
jebryce <jbryce@hawaii.edu>
2022-10-28 20:15:19 -1000

    improved docs, reading; call to sdnParserDesc

--- functions/sdnParser.py
+++ functions/sdnParser.py
@@ -1,4 +1,33 @@
-import library.EtherType_dict as EtherType_dict
+# Given a packet with type 'bytes', we want to be able to fully parse the 
+# packet down to what every byte does. 
+#
+# This file contains a main class 'Packet' which parsers the very start of the 
+# packet (the Ethernet frame) up to the EtherType field. Then the main class 
+# creates a subclass for whatever EtherType the packet was, and that subclass 
+# continues parsing the packet until a variable is assigned to every byte.
+#
+# This file also contains all ethertype subclasses the main 'Packet' class 
+# creates.
+# 
+# Also, every class that is defined in this file, has an individual description 
+# class created with it, which are defined 
+# at /functions/sdnParserDescriptions.py, see for more detail
+#
+# A way to call the class's variables, for example:
+# my_Packet = Packet(bytes), where bytes is a packet of type bytes
+# my_Packet.ethertype                  | returns a bytes object
+#                                      | ex: 0806 (type: bytes)
+# my_Packet.desc.ethertype             | returns a string that describes
+#                                      | ex: 'Address Resolution Protocol (ARP)'
+# my_Packet.arp.opcode                 | returns a bytes object
+#                                      | ex: 01 (type: bytes)
+# my_Packet.arp.desc.opcode            | returns a string that describes
+#                                      | ex: 'Request' (type: string)
+# generally:                           |
+# my_Packet.variable                   | calls the data of variable
+# my_Packet.desc.variable              | calls the description of variable
+# 
+from functions import sdnParserDescriptions
 
 class Packet:
     def __init__(self, packet):
@@ -10,8 +39,9 @@ class Packet:
         self.destination_mac_address = packet[0:6]
         self.source_mac_address = packet[6:12]
 
-        # Check if tagged traffic 
-        # 8100 is the EtherType 802.1Q (aka tagged traffic)
+        # Check if tagged traffic, 
+        # the .hex() function converts the bytes object to a string of hex 
+        # values, 8100 is the EtherType 802.1Q (aka tagged traffic)
         if packet[12:14].hex()=='8100':
             # Decided to use a boolean variable as it is either tagged or not 
             self.tagged = True
@@ -21,70 +51,64 @@ class Packet:
 
             # the rest of the packet, this is to avoid a seperate variable to 
             # keep track of the differing index due to the 802.1Q header
-            etherTypePacket = packet[16:]
+            partialPacket = packet[16:]
         else:
             self.tagged = False
-            etherTypePacket = packet[12:]
-
-
-        self.findEtherType(etherTypePacket)
-
-
-    def findEtherType(self, partialPacket):
-        # decided to make this a seperate function in case we get a packet with 
-        # an Ether Type I haven't made a class for yet
+            partialPacket = packet[12:]
 
         # ex: 0806 (type: bytes)
         self.ethertype = partialPacket[0:2]
 
-        # looks up ethertype value in dictionary to find description
-        # ex: 'Address Resolution Protocol (ARP)' (type: string)
-        self.ethertype_desc = EtherType_dict.ethertypes.get(
-            self.ethertype.hex()
-        )                                            
-        # if the key in dictionary.get(key) does not exist, it returns None
 
-
-        # this will probably never get called, but essentially does the same as 
-        # previous method, but for rarer ranged ethertypes.
-        if self.ethertype_desc == None:
-            for key in EtherType_dict.ranged_ethertypes:
-
-                # if the upper value of the range is greater than the ethertype
-                # this works as the dictionary is in incremental order
-                if bytes.fromhex(key[5:]) >= self.ethertype:
-
-                    # \ character indicates expression is on next line
-                    self.ethertype_desc = \
-                        EtherType_dict.ranged_ethertypes.get(key)
-                    break
-            
-    
-        # will replace laters
+        # I couldn't think of a better way to do this (that was as readable)
+        # this creates a new class object that then parsers the rest of the 
+        # packet. If nothing is found, the variables 
+        # self.ethertype and self.ethertype_desc (should) always have values  
         if self.ethertype.hex() == '0806':
             self.arp = arp(partialPacket[2:])
         elif self.ethertype.hex() == '0800':
             self.ipv4 = ipv4(partialPacket[2:])
 
-class arp(Packet):
-    def __init__(self, arp_packet):
-        # Decided to chop off the front of the packet as tagged traffic would have different index values
-        self.arp_packet = arp_packet
+        # see sdnParserDescriptions for more detail, but it creates 
+        # descriptions for a self.variable at self.desc.variable 
+        self.desc = sdnParserDescriptions.PacketDesc(self)
 
-        self.hardware_type = self.arp_packet[0:2]
-        self.protocol_type = self.arp_packet[2:4]
+
+
+class arp:
+    def __init__(self, arp_packet):
+        # Decided to chop off the front of the packet as tagged traffic would 
+        # have different index values
+
+        # ex: 0001 (type: bytes)
+        self.hardware_type = arp_packet[0:2]
+
+        # this shares the same EtherType numbers/descriptions
+        # ex: 0800 (type: bytes)
+        self.protocol_type = arp_packet[2:4]
 
         # index [4:5] returns a bytes object, index[5] returns an integer
-        self.hardware_size = self.arp_packet[4:5]
-        self.protocol_size = self.arp_packet[5:6]
+        # ex: 06 (type: bytes)
+        self.hardware_size = arp_packet[4:5]
 
-        self.opcode = self.arp_packet[6:8]
+        # ex: 04 (type: bytes)
+        self.protocol_size = arp_packet[5:6]
 
-        self.sender_mac_address = self.arp_packet[8:14]
-        self.sender_ip_address = self.arp_packet[14:18]
+        # ex: 0001 (type: bytes)
+        self.opcode = arp_packet[6:8]
 
-        self.target_mac_address = self.arp_packet[18:24]
-        self.target_ip_address = self.arp_packet[24:28]
+        # ex: B8 27 EB 3C 2D 60 (type: bytes)
+        self.sender_mac_address = arp_packet[8:14]
 
-class ipv4(Packet):
+        # ex: A9 FE B2 34 (type: bytes)
+        self.sender_ip_address = arp_packet[14:18]
+
+        # ex: 00 00 00 00 00 00 (type: bytes)
+        self.target_mac_address = arp_packet[18:24]
+
+        # ex: 80 AB 01 01 (type: bytes)     
+        self.target_ip_address = arp_packet[24:28]
+
+
+class ipv4:
     pass
