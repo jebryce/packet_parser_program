a419abe174f0426fd7610487de24553ef1b03a5c
jebryce <jbryce@hawaii.edu>
2022-12-11 21:07:48 -1000

    readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Packet Parser Program (PPP)
 A Python based network packet analyzer/parser used in the SCADA lab at the University of Hawaii at Manoa.
+
+See control.py for my testing control setup.
--- control.py
+++ control.py
@@ -1,7 +1,7 @@
 #   In order to write this parser at home, I needed bytes objects (in the 
 # formatting as seen in lab). The easiest way I found to do this was to save 
 # the bytes object as a string (using bytes.hex() function) to a text file. 
-# There are 100% better ways to do this, but this was for testing only and is 
+# There are better ways to do this, but this was for testing only and is 
 # not meant for production use.
 #
 # The sdnParser and printPKTinfo libraries will be used just as 
@@ -15,18 +15,21 @@ from PPP.functions import print_Packet
 from PPP.functions import Packet_Parser
 from PPP.functions import make_library
 import os
+
+# uncomment this make_library function to create files at ~/Documents/PPP/ that 
+# contain descriptions for MAC addresses and other data values
 # make_library.make_library()
 
 
 
-
-packets = open('PPP/library/test_cases.txt','r').readlines()
+location = 'PPP/library/test_cases.txt'
+packets = open(location,'r').readlines()
 
 
 # for each line in the text file, 
 # ex hex_string = 'ff59a300c763a8f2' (type: string)
-for hex_string in packets[:]:
-    os.system('cls')
+for hex_string in packets:
+    os.system('clear')
 
     # convert the line into it's intended bytes object
     pkt = bytes.fromhex(hex_string)
@@ -37,9 +40,8 @@ for hex_string in packets[:]:
     # print the objects information to console
     print_Packet.Printer(Packet)
 
-    # to make sure I am deleting large description dictionaries
-    # print(psutil.Process().memory_info().rss / (1024*1024))
-
+    input('Press enter to continue.')
+print('End of file reached.')
 
 
 
